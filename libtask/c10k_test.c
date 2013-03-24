1815ebe1ff582c23dd384a64013ce1372e7a950d
BVK Chaitanya <bvkchaitanya@gmail.com>
2013-03-23 17:10:42 -0700

    Added GPLv3 header to all files.

--- libtask/c10k_test.c
+++ libtask/c10k_test.c
@@ -1,4 +1,23 @@
 //
+// Libtask: A thread-safe coroutine library.
+//
+// Copyright (C) 2013  BVK Chaitanya
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+//
 // Testcase that simulates the c10k challenge.
 //
 // 1. We use two task-pools one for io and another for CPU to create
